847049907b4d058631bcf46a2dde5c20efe44dc6
Kevin Flores Garcia <kevinfg1413@gmail.com>
2019-10-22 13:40:02 -0600

    Inicial.

---
+++ src/airport_p3/Airport_P3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airport_p3;
+
+/**
+ *
+ * @author Kevin Flores
+ */
+public class Airport_P3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
